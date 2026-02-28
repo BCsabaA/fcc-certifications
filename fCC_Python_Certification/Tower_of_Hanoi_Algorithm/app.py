aaba56104b5877ca6334b59d50b0a32b5e63aab3
BCsabaA <barabas.csabi@gmail.com>
2026-02-28 16:08:29 +0100

    wip: hanoi

--- fCC_Python_Certification/Tower_of_Hanoi_Algorithm/app.py
+++ fCC_Python_Certification/Tower_of_Hanoi_Algorithm/app.py
@@ -1,29 +1,48 @@
 def hanoi(n: int) -> str:
+    hanoi_str = ''
+    step = 1
 
-    def hanoi_solve(n, source: list, target: list, auxiliary: list) -> None:
-        print(f'{source   =}')
-        print(f'{auxiliary=}')
-        print(f'{target   =}')
+    def hanoi_solve(n, source: list, target: list, auxiliary: list, mode: 1) -> None:
+        nonlocal step
+        nonlocal hanoi_str
+        # print(f'{source} {auxiliary} {target}')
         if n == 1:
+            print(f'{step=}')
+            print(f'{mode=}')
+            print(hanoi_str)
             source.pop(-1)
-            target.append(n)
+            target.append(1)
+            step += 1
+            if mode == 1:
+                hanoi_str = str(source) + ' ' + str(auxiliary) + ' ' + str(target)
+            if mode == 2:
+                hanoi_str = str(source) + ' ' + str(target) + ' ' + str(auxiliary)
+            if mode == 3:
+                hanoi_str = str(auxiliary) + ' ' + str(source) + ' ' + str(target)
             return
-        hanoi_solve(n-1, source, auxiliary, source)
-        hanoi_solve(n-1, auxiliary, target, source)
+        hanoi_solve(n-1, source, auxiliary, target, 2)
+        print(f'{step=}')
+        print(f'{mode=}')
+        print(hanoi_str)
+        source.pop(-1)
+        target.append(n)
+        step += 1
+        if mode == 1:
+            hanoi_str = str(source) + ' ' + str(auxiliary) + ' ' + str(target)
+        if mode == 2:
+            hanoi_str = str(source) + ' ' + str(target) + ' ' + str(auxiliary)
+        if mode == 3:
+            hanoi_str = str(auxiliary) + ' ' + str(source) + ' ' + str(target)
+        hanoi_solve(n-1, auxiliary, target, source, 3)
     
     first = list(range(n, 0, -1))
     second = []
     third = []
-    print('start')
-    print(f'{first =}')
-    print(f'{second=}')
-    print(f'{third =}')
 
-    hanoi_solve(n, first, third, second)
+    hanoi_str = str(first) + ' ' + str(second) + ' ' + str(third)
 
-    print('finish')
-    print(f'{first =}')
-    print(f'{second=}')
-    print(f'{third =}')
+    hanoi_solve(n, first, third, second, 1)
 
-hanoi(2)
+    print(hanoi_str)
+
+hanoi(3)
--- fCC_Python_Certification/Tower_of_Hanoi_Algorithm/main.py
+++ fCC_Python_Certification/Tower_of_Hanoi_Algorithm/main.py
@@ -19,27 +19,48 @@ def hanoi_solver(n: int) -> str:
     def move_to_str(move: list) -> str:
         return " ".join(str(s) for s in move)+'\n'
 
+    def move(from_list: list, to_list: list, last_item) -> int:
+        if len(from_list) == 0:
+            return -1
+        item_to_move = from_list[-1]
+        if item_to_move == last_item:
+            return -1
+        if len(from_list) > 1 and len(to_list) > 0:
+            if from_list[-2] == to_list[-1] - 1:
+                return -1
+        if to_list == [] or item_to_move < to_list[-1]:
+            from_list.pop(-1)
+            to_list.append(item_to_move)
+            return item_to_move
+        return -1
+
     def next_move(hanoi_list: list, last_item) -> int:
         print(f'{last_item=}')
         # nem mozoghat az utolsó mozgatott elem
-        left = hanoi_list[0]
-        middle = hanoi_list[1]
-        right = hanoi_list[2]
-        print(left)
-        print(middle)
-        print(right)
-        # kezdés:
-        #     páratlan az elsőt a harmadikra
-        #     páros az elsőt a másodikra
-        # az elsôt az harmadikra, hanemlehetakkor
+        first = hanoi_list[0]
+        second = hanoi_list[1]
+        third = hanoi_list[2]
+        print(f'{first=}')
+        print(f'{second=}')
+        print(f'{third=}')
+        # az elsőt a harmadikra, hanemlehetakkor
+        item = move(first, third, last_item)
         # az elsőt a másodikra, hanemlehetakkor
+        if item == -1:
+             item = move(first, second, last_item)
         # a másodikat a harmadikra, hanemlehetakkor
+        if item == -1:
+             item = move(second, third, last_item)
         # a harmadikat a másokdikra, hanemlehetakkor
+        if item == -1:
+             item = move(third, second, last_item)
         # a másodikat az elsőre, hanemlehetakkor
-        # a harmadikat az elsőre
-        
+        if item == -1:
+             item = move(second, first, last_item)
+        return item
             
-        
+    
+    step = 1
     hanoi_list = set_hanoi_list(n)
     start_list = hanoi_list[0].copy()
     hanoi_solve_moves = move_to_str(hanoi_list)
@@ -48,13 +69,17 @@ def hanoi_solver(n: int) -> str:
 
     escape = ''
     print(f'{start_list=}\n')
+    print(f'{step=}')
     print(hanoi_solve_moves)
+    step += 1
     last_item_moved = 0
 
     while start_list != hanoi_list[-1]:
         last_item_moved = next_move(hanoi_list, last_item_moved)
         hanoi_solve_moves += move_to_str(hanoi_list)
+        print(f'{step=}')
         print(hanoi_solve_moves)
+        step += 1
         print(f'{escape=}')
         print(f'{last_item_moved=}')
         escape = input('q for quit, any other to continue:  ')
@@ -67,13 +92,13 @@ def hanoi_solver(n: int) -> str:
 
 
 
-hanoi_solver(3)
+hanoi_solver(4)
 # hanoi_solver(4)
 # hanoi_solver(5)
 # hanoi_solver(6)
 # print()
 # print('[3, 2, 1] [] []\n[3, 2] [] [1]\n[3] [2] [1]\n[3] [2, 1] []\n[] [2, 1] [3]\n[1] [2] [3]\n[1] [] [3, 2]\n[] [] [3, 2, 1]')
-# print()
-# print('[4, 3, 2, 1] [] []\n[4, 3, 2] [1] []\n[4, 3] [1] [2]\n[4, 3] [] [2, 1]\n[4] [3] [2, 1]\n[4, 1] [3] [2]\n[4, 1] [3, 2] []\n[4] [3, 2, 1] []\n[] [3, 2, 1] [4]\n[] [3, 2] [4, 1]\n[2] [3] [4, 1]\n[2, 1] [3] [4]\n[2, 1] [] [4, 3]\n[2] [1] [4, 3]\n[] [1] [4, 3, 2]\n[] [] [4, 3, 2, 1]')
+print()
+print('[4, 3, 2, 1] [] []\n[4, 3, 2] [1] []\n[4, 3] [1] [2]\n[4, 3] [] [2, 1]\n[4] [3] [2, 1]\n[4, 1] [3] [2]\n[4, 1] [3, 2] []\n[4] [3, 2, 1] []\n[] [3, 2, 1] [4]\n[] [3, 2] [4, 1]\n[2] [3] [4, 1]\n[2, 1] [3] [4]\n[2, 1] [] [4, 3]\n[2] [1] [4, 3]\n[] [1] [4, 3, 2]\n[] [] [4, 3, 2, 1]')
 # print()
 # print('[5, 4, 3, 2, 1] [] []\n[5, 4, 3, 2] [] [1]\n[5, 4, 3] [2] [1]\n[5, 4, 3] [2, 1] []\n[5, 4] [2, 1] [3]\n[5, 4, 1] [2] [3]\n[5, 4, 1] [] [3, 2]\n[5, 4] [] [3, 2, 1]\n[5] [4] [3, 2, 1]\n[5] [4, 1] [3, 2]\n[5, 2] [4, 1] [3]\n[5, 2, 1] [4] [3]\n[5, 2, 1] [4, 3] []\n[5, 2] [4, 3] [1]\n[5] [4, 3, 2] [1]\n[5] [4, 3, 2, 1] []\n[] [4, 3, 2, 1] [5]\n[1] [4, 3, 2] [5]\n[1] [4, 3] [5, 2]\n[] [4, 3] [5, 2, 1]\n[3] [4] [5, 2, 1]\n[3] [4, 1] [5, 2]\n[3, 2] [4, 1] [5]\n[3, 2, 1] [4] [5]\n[3, 2, 1] [] [5, 4]\n[3, 2] [] [5, 4, 1]\n[3] [2] [5, 4, 1]\n[3] [2, 1] [5, 4]\n[] [2, 1] [5, 4, 3]\n[1] [2] [5, 4, 3]\n[1] [] [5, 4, 3, 2]\n[] [] [5, 4, 3, 2, 1]')
