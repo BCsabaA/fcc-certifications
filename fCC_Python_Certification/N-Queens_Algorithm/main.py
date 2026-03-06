3bf19bb166a6a735ccaf5cf377f8ca1e49bc31a2
BCsabaA <barabas.csabi@gmail.com>
2026-03-07 00:27:48 +0100

    wip: N-Queen

--- fCC_Python_Certification/N-Queens_Algorithm/main.py
+++ fCC_Python_Certification/N-Queens_Algorithm/main.py
@@ -71,23 +71,164 @@ def print_matrix(matrix):
 
 
 def dfs_n_queens(n):
-    if n == 1:
-        return
+    if n < 1:
+        return []
+    
     adj_matrix = []
     for i in range(n):
         adj_matrix.append([0] * n)
     solutions = []
     solution = []
-    for y in range(n):
-        for x in range(n):
+
+    start_coord = (0,0)
+    stack = [start_coord]
+    visited = [start_coord]
+
+    print_matrix(adj_matrix)
+
+    while stack:
+        print(f'{start_coord=}')
+        print(f'{stack=}')
+        actual_coord_x, actual_coord_y = stack.pop()
+        print(f'{visited=}')
+        print(f'{actual_coord_x=}')
+        print(f'{actual_coord_y=}')
+
+        if matrix_data_of_coord(adj_matrix, (actual_coord_x, actual_coord_y))['total'] == 0:
+            adj_matrix[actual_coord_y][actual_coord_x] = 1
             print_matrix(adj_matrix)
-            print(solution)
-            if matrix_data_of_coord(
-                    adj_matrix, (x, y))['total'] == 0:
-                solution.append(y)
-                adj_matrix[x][y] = 1
-                break
-    return solution
+            solution.append(actual_coord_y)
+
+        if actual_coord_y + 1 < len(adj_matrix):
+            new_coord_y = actual_coord_y + 1
+            new_coord_x = actual_coord_x
+            new_coord = (new_coord_x, new_coord_y)
+            print(f'{new_coord=}')
+            stack.append(new_coord)
+            visited.append(new_coord)
+        else:
+            if actual_coord_x + 1 < len(adj_matrix):
+                new_coord_x = actual_coord_x + 1
+                new_coord_y = 0
+                new_coord = (new_coord_x, new_coord_y)
+                print(f'{new_coord=}')
+                stack.append(new_coord)
+                visited.append(new_coord)
+            else:
+                print(f'{solution=}')
+                if len(solution) == n:
+                    solutions.append(solution)
+                print(f'{solutions=}')
+                if start_coord[1] + 1 < len(adj_matrix):
+                    start_coord = (start_coord[0], start_coord[1] + 1)
+                    print(f'new {start_coord=}')
+                    stack = [start_coord]
+                    visited = [start_coord]
+                else:
+                    return solutions
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     print(f'{stack=}')
+    #     print(f'{visited=}')
+    #     act_coord = stack.pop()
+    #     print(f'{act_coord=}')
+    #     print_matrix(adj_matrix)
+    #     act_coord_x = act_coord[0]
+    #     act_coord_y = act_coord[1]
+    #     if matrix_data_of_coord(adj_matrix, act_coord)['total'] == 0:
+    #         adj_matrix[act_coord_y][act_coord_x] = 1
+    #         solution.append(act_coord_y)
+    #     new_coord_x = 0
+    #     new_coord_y = 0
+    #     print(f'{len(adj_matrix)=}')
+    #     print(f'{act_coord_x=}')
+    #     print(f'{act_coord_y=}')
+
+    #     if act_coord_x + 1 < len(adj_matrix):
+    #         new_coord_x = act_coord_x + 1
+    #         new_coord_y = act_coord_y
+    #         print(f'{new_coord_x=}')
+    #     else:
+    #         new_coord_x = 0
+    #         if act_coord_y + 1 < len(adj_matrix):
+    #             new_coord_y = act_coord_y + 1
+    #             print(f'{new_coord_y=}')
+    #         else:
+    #             stack = []
+    #             visited = []
+    #             if len(solution) == n:
+    #                 solutions.append(solution)
+    #             solution = []
+    #             start_coord_x = start_coord[0]
+    #             start_coord_y = start_coord[1]
+    #             if start_coord_x + 1 < len(adj_matrix):
+    #                 new_coord_x = start_coord_x + 1
+    #             else:
+    #                 start_coord = (start_coord_x, start_coord_y)
+    #                 break
+    #             new_coord_y = 0
+    #     new_coord = (new_coord_x, new_coord_y)
+    #     print(f'{new_coord=}')
+    #     if new_coord not in visited:
+    #         stack.append(new_coord)
+    #         visited.append(new_coord)
+            
+    # # if len(solution) == n:
+    # #     solutions.append(solution)
+
+
+
+    # # for y in range(n):
+    # #     for x in range(n):
+    # #         print_matrix(adj_matrix)
+    # #         print(solution)
+    # #         if matrix_data_of_coord(
+    # #                 adj_matrix, (x, y))['total'] == 0:
+    # #             solution.append(y)
+    # #             adj_matrix[x][y] = 1
+    # #             break
+    # # return solution
 
 n = 4
 
